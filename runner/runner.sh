e2ffd5ddc5d238392eaff6eb2361fd6280b04c94
skyface <sjoerz@skyface.de>
2023-06-29 22:42:50 +0200

    Fix newline in curl

--- runner/runner.sh
+++ runner/runner.sh
@@ -4,7 +4,8 @@
 # Load the API endpoint and API key from the source file
 # TODO: Make this configurable
 # source /etc/cron-runner/config.sh
-source ./config.sh
+MYDIR="$(dirname "$(realpath "$0")")"
+source "$MYDIR/config.sh"
 
 JOB_ID=$1
 # Shift the positional parameters to the left by 1 to get the command to run
@@ -30,6 +31,9 @@ BOOL="true"
 if [ $? -ne 0 ]; then
     BOOL="false"
 fi
+# Remove all newlines from the output
+OUTPUT=$(echo "${OUTPUT}" | tr -d '\n')
+
 
 # Send the output to the API endpoint, with the API key and job ID and a boolean indicating whether the command was successful
 curl -X POST -H "Content-Type: application/json" -d "{\"api_key\":\"${API_KEY}\",\"job_id\":\"${JOB_ID}\",\"output\":\"${OUTPUT}\",\"success\":${BOOL}}" ${API_ENDPOINT}
